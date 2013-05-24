2e971712dc278d34ec4bde2fc29b1e4ea5a53243
Arti Mathanda <artismarti@gmail.com>
2013-05-24 13:02:42 +0100

    the 3 supermodels.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ActiveRecord::Base
   attr_accessible :address_1, :address_2, :city, :email, :name, :postcode, :strip_card_token, :stripe_customer_token
+
+  has_many :order_products
+  has_many :products, through: :order_products
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :address_1, presence: true
+  validates :address_2, presence: true
 end
