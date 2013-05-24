fb3ffa7653f2433e373c0b4c53a332a1f7fc7057
Arti Mathanda <artismarti@gmail.com>
2013-05-24 12:42:43 +0100

    cleaned products page on AA

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,22 @@
 ActiveAdmin.register Product do
-  
+	form do |f|
+			f.inputs do
+			f.input :title
+			f.input :description
+			f.input :price_in_pence
+			f.input :previous_price_in_pence
+			f.input :image, hint: (image_tag(f.object.image(:thumb)) if f.object.image?)
+		end
+		f.buttons
+	end
+
+	index do
+		id_column
+		column :title
+		column :description
+		column :previous_price_in_pence
+		column :previous_price_in_pence
+		column :image
+  		default_actions
+  	end
 end
