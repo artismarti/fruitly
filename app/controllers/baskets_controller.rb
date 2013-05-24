f835281b77f7b3d52cb0be4686f7c65ed4914ddb
Arti Mathanda <artismarti@gmail.com>
2013-05-24 13:02:15 +0100

    basket & product controller

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,2 +1,18 @@
 class BasketsController < ApplicationController
+	def create
+		@product = Product.find(params[:product_id])
+		basket.add(@product.id)
+
+		flash[:success] = "added #{@product.title} to basket"
+		redirect_to product_path(@product)
+	end
+
+	def destroy
+		@product = Product.find(params[:product_id])
+		basket.delete(@product.id)
+
+		flash[:success] = "removed #{@product.title} from basket"
+		redirect_to product_path(@product)		
+	end
+
 end
