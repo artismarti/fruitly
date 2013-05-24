63a8b7e1c4dcea2a2746d8aa5d5baa62adb9c449
Arti Mathanda <artismarti@gmail.com>
2013-05-24 11:21:31 +0100

    moved AA devise to bottom of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Fruitly::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
   resources :orders
   resources :products do
     resources :basket
   end
 
   root to: "products#index"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
